d9a95767bf8b3f6ff0a710c3d61a705748d15fde
Mihara3389 <m_mihara@emyus.jp>
2021-05-10 10:26:22 +0900

    指摘変更：不要な変数定義削除

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,9 @@
 <?php
 //データベース接続
-require_once 'env.php';
-$host = DB_HOST;
-$dbname = DB_NAME;
-$dbuser = DB_USER;
-$dbpass = DB_PASS;
+$host = 'localhost';
+$dbname = 'phpApp';
+$dbuser = 'hoge';
+$dbpass = 'himitu';
 
 $dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
 //PDOの例外エラーを詳細にしてくれるオプションとselect文やwhere句などの結果を連想配列として返してくれるオプション
--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-//データベースの接続情報
-define('DB_HOST' ,'localhost');
-define('DB_NAME' ,'phpApp');
-define('DB_USER' ,'hoge');
-define('DB_PASS' ,'himitu');
-?>
