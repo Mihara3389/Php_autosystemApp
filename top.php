126bb87322e0126e00dbef874155d7a98a70bbef
Mihara3389 <m_mihara@emyus.jp>
2021-05-11 17:18:37 +0900

    テスト問題の表示実装

--- top.php
+++ top.php
@@ -5,7 +5,7 @@ $err = [];
 //変数初期化
 $members =[];
 $qadata = [];
-$merge = [];
+$result = '';
 //ログインセッションを確認
 if (isset($_SESSION['id']))
  {
@@ -65,8 +65,22 @@ if (isset($_SESSION['id']))
         }
     }else if(isset($_POST["test"]))
     {
+        //questionデータを取得する
+        $sql = "SELECT DISTINCT questions.id as qid, questions.question as q FROM questions INNER JOIN correct_answers ON questions.id = correct_answers.question_id;";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        while ($qa  = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $qadata[] = $qa;
+        }
+        $result = shuffle($qadata);
         //テスト画面へ遷移
-        header("location: test_form.php");
+        if ($result === false) {
+            //新規qa登録画面へ遷移
+            header("location: register_form.php");
+        }else{
+            $_SESSION['testlist'] = $qadata;
+            header("location: test_form.php");
+        }
     }else if(isset($_POST["history"]))
     {
         //ログインユーザーのデータをhistoriesより取得する
